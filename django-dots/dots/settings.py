daa7e3755770517a4946763144f800623c1ad6ca
Rissap <riisa.toro@gmail.com>
2020-12-04 12:29:31 +0200

    db setup

--- django-dots/dots/settings.py
+++ django-dots/dots/settings.py
@@ -74,30 +74,17 @@ WSGI_APPLICATION = 'dots.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    }
-}
-'''
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "dots",
-        'USER': "postgres",
-        'PASSWORD': "postgres",
-        'HOST': "postgres",
-        'PORT': 5432,
+        'NAME': os.getenv("DB_NAME", "dots"),
+        'USER': os.getenv("DB_USER", "postgres"),
+        'PASSWORD': os.getenv("DB_PASSWORD", "postgres"),
+        'HOST': os.getenv("DB_HOST", "postgres"),
+        'PORT': os.getenv("DB_PORT", 5432),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
