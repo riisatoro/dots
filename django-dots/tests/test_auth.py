bc79648b120762914254adc05e1ba64bcf9bb564
Rissap <riisa.toro@gmail.com>
2020-12-04 11:02:27 +0200

    login tests added

--- django-dots/tests/test_auth.py
+++ django-dots/tests/test_auth.py
@@ -7,24 +7,34 @@ from django.contrib.auth.models import User
 class LoginTest(TestCase):
     def setUp(self):
         self.client = Client()
-        
+        self.url = "/api/auth/login/"
+        self.content = "application/json"
+
         self.user = User(username="admin")
         self.user.save()
         self.user.set_password("admin")
         self.user.save()
 
-    def corret_login(self):
-        response = self.client.post("/api/auth/login/", {"username": "admin", "password": "admin"}, content_type="application/json")
+    def test_corret_login(self):
+        response = self.client.post(self.url, {"username": "admin", "password": "admin"}, content_type=self.content)
         reply = json.loads(response.content.decode('utf8').replace("'", '"'))
 
         self.assertEqual(response.status_code, 200)
         self.assertFalse(reply["error"])
 
-    def wrong_username(self):
-        pass
+    def test_wrong_username(self):
+        response = self.client.post(self.url, {"username": "awdawdawd", "password": "admin"}, content_type=self.content)
+        reply = json.loads(response.content.decode('utf8').replace("'", '"'))
 
-    def wrong_password(self):
-        pass
+        self.assertEqual(response.status_code, 401)
+        self.assertContains("Invalid login", reply["message"])
+
+    def test_wrong_password(self):
+        response = self.client.post(self.url, {"username": "awdawdawd", "password": "admin"}, content_type=self.content)
+        reply = json.loads(response.content.decode('utf8').replace("'", '"'))
+
+        self.assertEqual(response.status_code, 401)
+        self.assertContains("Invalid password", reply["message"])
 
 
 class RegistrationTest(TestCase):
