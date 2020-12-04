350a0cbde4088eee7c8c11928fdbbb85acf32893
Rissap <riisa.toro@gmail.com>
2020-12-04 11:08:43 +0200

    login tests added

---
+++ django-dots/api/tests.py
@@ -0,0 +1,55 @@
+import json
+from django.test import Client
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+
+class LoginTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = "/api/auth/login/"
+        self.content = "application/json"
+
+        self.user = User(username="admin")
+        self.user.save()
+        self.user.set_password("admin")
+        self.user.save()
+
+    def test_corret_login(self):
+        response = self.client.post(self.url, {"username": "admin", "password": "admin"}, content_type=self.content)
+        reply = json.loads(response.content.decode('utf8').replace("'", '"'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(reply["error"])
+
+    def test_wrong_username(self):
+        response = self.client.post(self.url, {"username": "awdawdawd", "password": "admin"}, content_type=self.content)
+        reply = json.loads(response.content.decode('utf8').replace("'", '"'))
+
+        self.assertEqual(response.status_code, 401)
+        self.assertContains("Invalid login", reply["message"])
+
+    def test_wrong_password(self):
+        response = self.client.post(self.url, {"username": "awdawdawd", "password": "admin"}, content_type=self.content)
+        reply = json.loads(response.content.decode('utf8').replace("'", '"'))
+
+        self.assertEqual(response.status_code, 401)
+        self.assertContains("Invalid password", reply["message"])
+
+
+class RegistrationTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def correct_registration(self):
+        pass
+
+    def registration_data_already_exists(self):
+        pass
+
+    def invalid_email(self):
+        pass
+
+    def different_passwords(self):
+        pass
+
