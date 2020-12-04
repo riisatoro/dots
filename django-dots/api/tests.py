84e40e62a7d237755af6c4c546ecc1899203ddec
Rissap <riisa.toro@gmail.com>
2020-12-04 12:54:03 +0200

    changed tests location

--- django-dots/api/tests.py
+++ django-dots/api/tests.py
@@ -44,3 +44,30 @@ class RegistrationTest(TestCase):
     def different_passwords(self):
         pass
 
+
+class SaveMatch():
+    def setUp(self):
+        self.client = Client()
+
+    def correct_save(self):
+        pass
+
+    def unauthorized_create(self):
+        pass
+
+    def value_missed(self):
+        pass
+
+    def equal_score(self):
+        pass
+
+
+class GetMatch(TestCase):
+    def setUp(self):
+        pass
+
+    def correct_request(self):
+        pass
+
+    def unauthorized_request(self):
+        pass
