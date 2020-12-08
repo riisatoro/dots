3c1c1ddaced19ceeafbbb41d0e4f68b8edd7c96f
Rissap <riisa.toro@gmail.com>
2020-12-08 13:29:27 +0200

    added test cases for game logic

--- django-dots/api/game/main.py
+++ django-dots/api/game/main.py
@@ -1,4 +1,14 @@
-""" Main logic of processing the game field, like, finding a loops and update captured points """
+""" Main logic of processing the game field, like, finding a loops and update captured points
+    :return {
+            "field": array of game point,
+            "is_full": boolean; true if field has no empty points,
+            "captured": array of amount of captured points
+            }
+"""
+
+from find_captured import process as captured
+from  full_field import process as full
+from calc_square import process as square
 
 def process(field, colors):
     player_1 = colors[0][0]
@@ -6,4 +16,8 @@ def process(field, colors):
 
 
 
-    return field
+    is_full = False
+    captured = [0, 0]
+
+    return {"field": field, "is_full": is_full, "captured": captured}
+
---
+++ django-dots/api/game/tests.py
@@ -0,0 +1,71 @@
+import unittest
+from main import process
+
+class TestStringMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.loop_field = [
+            ["E", "R", "R", "R", "R", "E", "E", "E", "E", "E", "E", "E", "G", "E", "E"],
+            ["R", "E", "E", "E", "E", "R", "E", "E", "E", "E", "E", "G", "E", "G", "E"],
+            ["R", "E", "E", "E", "E", "E", "R", "E", "E", "E", "E", "G", "E", "G", "E"],
+            ["R", "E", "E", "R", "R", "E", "R", "E", "E", "E", "E", "G", "E", "G", "E"],
+            ["R", "E", "R", "E", "E", "R", "R", "E", "E", "E", "E", "E", "G", "E", "E"],
+            ["E", "R", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"],
+            ["E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"],
+            ["E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"],
+            ["E", "E", "E", "R", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"],
+            ["E", "E", "E", "G", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"],
+            ["E", "E", "G", "R", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"],
+            ["R", "G", "R", "G", "R", "G", "R", "E", "E", "E", "E", "E", "E", "E", "E"],
+            ["E", "E", "G", "R", "G", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"],
+            ["E", "E", "E", "G", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"],
+            ["E", "E", "E", "R", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"]
+        ]
+        self.full_field = [["R"]*8]*8
+
+        self.captured_field = [
+            ["E", "R", "R", "R", "R", "E", "E", "E", "E", "E"],
+            ["R", "E", "E", "E", "E", "R", "E", "E", "E", "E"],
+            ["R", "G", "E", "E", "E", "E", "R", "E", "E", "E"],
+            ["R", "E", "E", "R", "R", "E", "R", "E", "E", "E"],
+            ["R", "E", "R", "E", "E", "R", "R", "E", "E", "E"],
+            ["E", "R", "E", "G", "E", "R", "E", "E", "E", "E"],
+            ["E", "E", "R", "R", "R", "E", "E", "G", "E", "E"],
+            ["E", "E", "E", "E", "E", "E", "G", "R", "G", "E"],
+            ["E", "E", "E", "E", "E", "E", "G", "R", "G", "E"],
+            ["E", "E", "E", "E", "E", "E", "E", "G", "E", "E"]
+        ]
+
+        self.colors = ['R', 'G']
+
+    def test_surrounded(self):
+        self.loop_field[2][2] = "R"
+        data = process(self.loop_field, self.colors)
+        new_field = data["field"]
+
+        self.assertEqual(new_field[2][2], 'R')
+        self.assertEqual(new_field[11][3], 'Gl')
+
+    def test_released(self):
+        self.loop_field[10][4] = ""
+        data = process(self.loop_field, self.colors)
+        new_field = data["field"]
+
+        self.assertEqual(new_field[11][3], 'G')
+        self.assertEqual(new_field[11][4], 'Rl')
+
+    def test_full_field(self):
+        data = process(self.full_field, self.colors)
+        is_full = data["is_full"]
+
+        self.assertTrue(is_full)
+
+    def test_captured(self):
+        data = process(self.captured_field, self.colors)
+
+        captured = data["captured"]
+        self.assertEqual(captured, [2, 2])
+
+
+if __name__ == '__main__':
+    unittest.main()
