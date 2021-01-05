c585f138a82bc3f0e3bacac52525c22f6b127bb9
Rissap <riisa.toro@gmail.com>
2021-01-05 16:44:43 +0200

    updatet Point object

--- django-dots/api/game/point.py
+++ django-dots/api/game/point.py
@@ -6,26 +6,15 @@ class Point():
     """
     Point: represents a point in the game field
     """
-    def __init__(self, color, part_of_loop=False, captured=None, loop_id=None):
-        self.color = color
-        self.part_of_loop = part_of_loop
-        self.captured = captured
-
-        self.loop_id = []
-        if loop_id:
-            self.loop_id = loop_id
+    def __init__(self, owner, captured=None):
+        self.owner = None
+        self.captured = []
+        if captured:
+            self.captured = captured
 
     def is_free(self):
-        """
-        Check if point is free - not captured and not in loop
-        """
-        return not self.captured and not self.part_of_loop
+        """ Check if point is free - not captured and not in loop """
+        return not self.captured and not self.owner
 
     def __str__(self):
         return f"{self.color}|loop: {self.part_of_loop}|captured: {self.captured}"
-
-    def __eq__(self, color):
-        return self.color == color
-
-    def __ne__(self, color):
-        return not self.color == color
